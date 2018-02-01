697502e5dcd6a9cf3bd2df80df9739ef22d36c1d
Qingyuan Feng <simonfengqy@gmail.com>
2018-02-01 13:15:44 -0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,22 @@
+"""
+Imports all submodules 
+"""
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
+#__version__ = '1.3.1'
+
+import dcCustom.data
+#import deepchem.feat
+#import deepchem.hyper
+#import deepchem.metalearning
+#import deepchem.metrics
+import dcCustom.models
+#import deepchem.nn
+#import deepchem.splits
+#import deepchem.trans
+#import deepchem.utils
+#import deepchem.dock
+#import deepchem.molnet
+#import deepchem.rl
