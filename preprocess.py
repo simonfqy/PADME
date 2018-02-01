5faa688a65dcfd110f2e6d4cddf7d34e3c4f302e
Qingyuan Feng <simonfengqy@gmail.com>
2018-02-01 13:28:43 -0800

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import argparse
+import os
+import sys
+import pwd
+import pdb
+import csv
+
+df = pd.read_csv('Bio_results.csv', header = 2, index_col=0, usecols=range(3, 76))
+#df = df.head(2)
+molList = list(df)
+#print(len(molList))
+protList = list(df.index)
+interactions = [] 
+for row in df.itertuples():
+    intxn = list(row)[1:]
+    interactions.append(intxn)  
+    #print(interactions)
+interactions = np.array(interactions)
+interactions[np.isnan(interactions)] = 10000
+interactions = 9- np.log10(interactions)
+with open('restructured.csv', 'w', newline='') as csvfile:
+    fieldnames = ['interaction_value', 'smiles', 'proteinName']
+    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer.writeheader()
+    for i, protein in enumerate(protList):
+        for j, compound in enumerate(molList):
+            # will start writing rows.
+            intxn_value = interactions[i][j]
+            #if  np.isnan(intxn_value):
+            #    intxn_value = 10000
+            #intxn_value = 9 - np.log10(intxn_value)
+            writer.writerow({'interaction_value': intxn_value, 'smiles': compound,
+                'proteinName': protein})     
+            
