55712c64b7fc3c5f65eac81a77edcc3abda099d8
Qingyuan Feng <simonfengqy@gmail.com>
2018-03-01 18:37:45 -0800

    Add files via upload

--- driver.py
+++ driver.py
@@ -19,16 +19,14 @@ import dcCustom
 from dcCustom.molnet.preset_hyper_parameters import hps
 from dcCustom.molnet.run_benchmark_models import model_regression, model_classification
 
+FLAGS = None
 
 def load_davis(featurizer = 'Weave', cross_validation=False, test=False, split='random', 
-  reload=True, K = 5, mode = 'regression', train_valid_only = True, predict_cold = False): 
+  reload=True, K = 5, mode = 'regression', predict_cold = False): 
   # The last parameter means only splitting into training and validation sets.
 
   if cross_validation:
     assert not test
-    assert train_valid_only
-
-  train_valid_only = not test
 
   if mode == 'regression' or mode == 'reg-threshold':
     mode = 'regression'
@@ -118,33 +116,33 @@ def load_prot_desc_dict(prot_desc_dict, prot_desc_path):
     descriptor = np.reshape(descriptor, (1, len(descriptor)))
     assert row[0] not in prot_desc_dict
     prot_desc_dict[row[0]] = descriptor    
-  #return prot_desc_dict
 
-def run_analysis(dataset='davis-metz', 
-                 featurizer = 'Weave',
-                 mode = 'regression',
-                 split= 'random',
-                 threshold = 7.0,
-                 direction = False,
-                 out_path = '.',
-                 fold_num = 5,
-                 hyper_parameters=None,
-                 hyper_param_search = False, 
-                 max_iter = 42,
-                 search_range = 3,
-                 reload = True,
-                 cross_validation = True,
-                 test = False,
-                 predict_cold = True, # Determines whether cold-start drugs and targets are tested or validated.
-                 early_stopping = True,
-                 evaluate_freq = 3, # Number of training epochs before evaluating
+def run_analysis(dataset=FLAGS.dataset, 
+                 featurizer = FLAGS.featurizer,
+                 mode = FLAGS.mode,
+                 split= FLAGS.split,
+                 threshold = FLAGS.threshold,
+                 direction = FLAGS.direction,
+                 out_path = FLAGS.out_path,
+                 fold_num = FLAGS.fold_num,
+                 hyper_parameters=FLAGS.hyper_parameters,
+                 hyper_param_search = FLAGS.hyper_param_search,
+                 max_iter = FLAGS.max_iter,
+                 search_range = FLAGS.search_range,
+                 reload = FLAGS.reload,
+                 cross_validation = FLAGS.cross_validation,
+                 test = FLAGS.test,
+                 predict_cold = FLAGS.predict_cold, # Determines whether cold-start 
+                 #drugs and targets are tested or validated.
+                 early_stopping = FLAGS.early_stopping,
+                 evaluate_freq = FLAGS.evaluate_freq, # Number of training epochs before evaluating
                  # for early stopping.
-                 patience = 3,
-                 seed=123,
-                 log_file = 'GPhypersearch_temp.log',
-                 model_dir = './model_dir',
-                 prot_desc_path=["davis_data/prot_desc.csv",
-                   "metz_data/prot_desc.csv"]):
+                 patience = FLAGS.patience,
+                 seed=FLAGS.seed,
+                 log_file = FLAGS.log_file,
+                 model_dir = FLAGS.model_dir,
+                 prot_desc_path=FLAGS.prot_desc_path):
+
   if mode == 'regression':
     metric = [deepchem.metrics.Metric(deepchem.metrics.rms_score, np.mean)]
   elif mode == 'classification':
@@ -172,7 +170,7 @@ def run_analysis(dataset='davis-metz',
   for path in prot_desc_path:
     load_prot_desc_dict(prot_desc_dict, path)
   prot_desc_length = 8421
-  pdb.set_trace()
+  #pdb.set_trace()
   
   # all_dataset will be a list of 5 elements (since we will use 5-fold cross validation),
   # each element is a tuple, in which the first entry is a training dataset, the second is
@@ -215,104 +213,63 @@ def run_analysis(dataset='davis-metz',
   
   opt_epoch = -1
   test_dataset = None
-  if mode == 'regression' or mode == 'reg-threshold':
-    if not cross_validation:
-      train_dataset, valid_dataset, test_dataset = all_dataset
-      train_score, valid_score, test_score, opt_epoch = model_regression(
-            train_dataset,
-            valid_dataset,
-            test_dataset,
-            tasks,
-            transformers,
-            n_features,
-            metric,
-            model,
-            prot_desc_dict,
-            prot_desc_length,
-            hyper_parameters=hyper_parameters,
-            test = test,         
-            early_stopping = early_stopping,
-            evaluate_freq = evaluate_freq, # Number of training epochs before evaluating
-            # for early stopping.
-            patience = patience,
-            direction=direction,
-            seed=seed,
-            model_dir=model_dir)
-      train_scores_list.append(train_score)
-      valid_scores_list.append(valid_score)
-      test_scores_list.append(test_score)
-    else:
-      for i in range(fold_num):
-        train_score, valid_score, _, _ = model_regression(
-            all_dataset[i][0],
-            all_dataset[i][1],
-            None,
-            tasks,
-            transformers,
-            n_features,
-            metric,
-            model,
-            prot_desc_dict,
-            prot_desc_length,
-            hyper_parameters=hyper_parameters,
-            test = test,
-            early_stopping = False,
-            direction=direction,
-            seed=seed,
-            model_dir=model_dir)
-
-        train_scores_list.append(train_score)
-        valid_scores_list.append(valid_score)
- 
-  elif mode == 'classification':
+  model_functions = {
+    'regression': model_regression,
+    'reg-threshold': model_regression,
+    'classification': model_classification
+  }
+  assert mode in model_functions
+  if mode == 'classification':
     direction=True
-    if not cross_validation:
-      train_dataset, valid_dataset, test_dataset = all_dataset
-      train_score, valid_score, test_score, opt_epoch = model_classification(
-            train_dataset,
-            valid_dataset,
-            test_dataset,
-            tasks,
-            transformers,
-            n_features,
-            metric,
-            model,
-            prot_desc_dict,
-            prot_desc_length,
-            hyper_parameters=hyper_parameters,
-            test = test,
-            early_stopping = early_stopping,
-            evaluate_freq = evaluate_freq, # Number of training epochs before evaluating
-            # for early stopping.
-            patience = patience,
-            direction=direction,
-            seed=seed,
-            model_dir=model_dir)
-      train_scores_list.append(train_score)
-      valid_scores_list.append(valid_score)
-      test_scores_list.append(test_score)
-    else:
-      for i in range(fold_num):
-        train_score, valid_score, _, _ = model_classification(
-            all_dataset[i][0],
-            all_dataset[i][1],
-            None,
-            tasks,
-            transformers,
-            n_features,
-            metric,
-            model,
-            prot_desc_dict,
-            prot_desc_length,
-            hyper_parameters=hyper_parameters,
-            test = test,
-            early_stopping = False,
-            direction=direction,
-            seed=seed,
-            model_dir=model_dir)
+  
+  if not cross_validation:
+    train_dataset, valid_dataset, test_dataset = all_dataset
+    train_score, valid_score, test_score, opt_epoch = model_functions[mode](
+          train_dataset,
+          valid_dataset,
+          test_dataset,
+          tasks,
+          transformers,
+          n_features,
+          metric,
+          model,
+          prot_desc_dict,
+          prot_desc_length,
+          hyper_parameters=hyper_parameters,
+          test = test,         
+          early_stopping = early_stopping,
+          evaluate_freq = evaluate_freq, # Number of training epochs before evaluating
+          # for early stopping.
+          patience = patience,
+          direction=direction,
+          seed=seed,
+          model_dir=model_dir)
+    train_scores_list.append(train_score)
+    valid_scores_list.append(valid_score)
+    test_scores_list.append(test_score)
+  else:
+    for i in range(fold_num):
+      train_score, valid_score, _, _ = model_functions[mode](
+          all_dataset[i][0],
+          all_dataset[i][1],
+          None,
+          tasks,
+          transformers,
+          n_features,
+          metric,
+          model,
+          prot_desc_dict,
+          prot_desc_length,
+          hyper_parameters=hyper_parameters,
+          test = test,
+          early_stopping = False,
+          direction=direction,
+          seed=seed,
+          model_dir=model_dir)
 
-        train_scores_list.append(train_score)
-        valid_scores_list.append(valid_score)
+      train_scores_list.append(train_score)
+      valid_scores_list.append(valid_score) 
+  
   time_finish_fitting = time.time()
   
   results_file = 'results'
@@ -364,8 +321,150 @@ def run_analysis(dataset='davis-metz',
   if hyper_param_search:
     with open(os.path.join(out_path, dataset + model + '.pkl'), 'w') as f:
       pickle.dump(hyper_parameters, f)
-  
+
 if __name__ == '__main__':
-  run_analysis()
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--dataset',
+      type=str,
+      default='davis',
+      help='Dataset name.'
+  )
+  parser.add_argument(
+      '--featurizer',
+      type=str,
+      default='Weave',
+      help='Name of the featurizer.'
+  )
+  parser.add_argument(
+      '--mode',
+      type=str,
+      default='regression',
+      help='Whether it is a regression problem, classification problem or thresholding problem.'
+  )
+  parser.add_argument(
+      '--split',
+      type=str,
+      default='random',
+      help='Which splitter to use.'
+  )
+  parser.add_argument(
+      '--threshold',
+      type=float,
+      default=7.0,
+      help='The threshold used in the mode named reg-threshold.'
+  )
+  parser.add_argument(
+      '--direction',
+      type=bool,
+      default=False,
+      help='The direction of desired metric values. False for minimization, True\
+        for maximization.'
+  )
+  parser.add_argument(
+      '--out_path',
+      type=str,
+      default='.',
+      help='The path of the repository that the output files should be stored.'
+  )
+  parser.add_argument(
+      '--fold_num',
+      type=int,
+      default=5,
+      help='Number of folds. Only useful when cross_validation is true.'
+  )
+  parser.add_argument(
+      '--hyper_parameters',
+      default=None,
+      help='Dictionary of hyper parameters.'
+  )
+  parser.add_argument(
+      '--hyper_param_search',
+      default=False,
+      help='Flag of whether hyperparameters will be searched.',
+      action='store_true'
+  )
+  parser.add_argument(
+      '--max_iter',
+      type=int,
+      default=42,
+      help='Maximum number of iterations for hyperparameter searching.'
+  )
+  parser.add_argument(
+      '--search_range',
+      type=int,
+      default=3,
+      help='Ratio of values to try in hyperparameter searching.'
+  )
+  parser.add_argument(
+      '--reload',
+      type=bool,
+      default=True,
+      help='Flag of whether datasets will be reloaded from existing ones or newly constructed.'
+  )
+  parser.add_argument(
+      '--cross_validation',
+      default=False,
+      help='Flag of whether cross validations will be performed.',
+      action='store_true'
+  )
+  parser.add_argument(
+      '--test',      
+      default=False,
+      help='Flag of whether there will be test data.',
+      action='store_true'
+  )
+  parser.add_argument(
+      '--predict_cold',      
+      default=False,
+      help='Flag of whether the split will leave "cold" entities in the test data.',
+      action='store_true'
+  )
+  parser.add_argument(
+      '--early_stopping',
+      type=bool,
+      default=False,
+      help='Flag of whether early stopping would be enabled. Does not apply to CV.',
+      action='store_true'
+  )
+  parser.add_argument(
+      '--evaluate_freq',
+      type=int,
+      default=3,
+      help='If enable early stopping, the number of training epochs before evaluation.'
+  )
+  parser.add_argument(
+      '--patience',
+      type=int,
+      default=3,
+      help='In early stopping, the number of evaluations without improvement that \
+        can be tolerated.'
+  )
+  parser.add_argument(
+      '--seed',
+      type=int,
+      default=123,
+      help='Random seed to be used in tensorflow.'
+  )
+  
+  parser.add_argument(
+      '--log_file',
+      type=str,
+      default='GPhypersearch_temp.log',
+      help='Name of the file storing the process of hyperparameter searching.'
+  )  
+  parser.add_argument(
+      '--model_dir',
+      type=str,
+      default='./model_dir',
+      help='Directory to store the log files in the training process. Can call\
+        tensorboard with it as the logdir.'
+  )
+  parser.add_argument(
+      '--prot_desc_path',
+      default=["davis_data/prot_desc.csv", "metz_data/prot_desc.csv"],
+      help='A list containing paths to protein descriptors.'      
+  )
 
-
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=run_analysis, argv=[sys.argv[0]] + unparsed)
