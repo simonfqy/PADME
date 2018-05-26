38d72a478d1cb98e138718290da538f27fb47e87
Qingyuan Feng <simonfengqy@gmail.com>
2018-05-25 19:29:07 -0700

    Add files via upload

--- dcCustom/feat/proteins.py
+++ dcCustom/feat/proteins.py
@@ -5,6 +5,8 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
+import re
+
 __author__ = "Qingyuan Feng"
 __copyright__ = "Copyright 2018, Simon Fraser University"
 __license__ = "MIT"
@@ -21,7 +23,21 @@ class Protein(object):
     
   def __eq__(self, other):
     if isinstance(self, other.__class__):
-      return (self.sequence == other.sequence)
+      if self.sequence == other.sequence:
+        if self.source == other.source:
+          return (self.name == other.name)
+        # We need to tackle the problem brought by phosphorylation in Davis data.
+        self_phospho_match = re.search('-phospho', self.name, re.I)
+        other_phospho_match = re.search('-phospho', other.name, re.I)
+        if self_phospho_match and other_phospho_match:
+          return True
+        # Here we made the assumption that those with '-nonphosphorylated' and those without
+        # any descriptions of phosphorylation are the same if they have the same sequence.
+        if not (self_phospho_match or other_phospho_match):
+          return True
+        return False
+      else:
+        return False
     return False
     
   def __hash__(self):
