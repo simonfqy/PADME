b8ff7428775b21f31493546c97cef3b44dfbec58
Qingyuan Feng <simonfengqy@gmail.com>
2018-06-01 22:17:26 -0700

    Add files via upload

--- davis_data/preprocess.py
+++ davis_data/preprocess.py
@@ -14,7 +14,7 @@ import pdb
 import csv
 
 df = pd.read_csv('Bio_results.csv', header = 2, index_col=0, usecols=range(3, 76))
-#df = df.head(2)
+#df = df.head(15000)
 molList = list(df)
 #print(len(molList))
 protList = list(df.index)
@@ -25,18 +25,26 @@ for row in df.itertuples():
     #print(interactions)
 interactions = np.array(interactions)
 interactions[np.isnan(interactions)] = 10000
-interactions = 9- np.log10(interactions)
-with open('restructured.csv', 'w', newline='') as csvfile:
-    fieldnames = ['interaction_value', 'smiles', 'proteinName']
+interactions = 9 - np.log10(interactions)
+interaction_bin = (interactions >= 7.0) * 1
+counter = 0
+with open('restructured_bin2.csv', 'w', newline='') as csvfile:
+    fieldnames = ['davis_bin', 'davis', 'smiles', 'proteinName']
     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
     writer.writeheader()
     for i, protein in enumerate(protList):
         for j, compound in enumerate(molList):
             # will start writing rows.
             intxn_value = interactions[i][j]
+            intxn_bin = interaction_bin[i][j]
             #if  np.isnan(intxn_value):
             #    intxn_value = 10000
-            #intxn_value = 9 - np.log10(intxn_value)
-            writer.writerow({'interaction_value': intxn_value, 'smiles': compound,
-                'proteinName': protein})     
+            writer.writerow({'davis_bin': intxn_bin, 'davis': intxn_value,
+              'smiles': compound, 'proteinName': protein})
+            counter += 1
+            if (counter > 2400):
+                break
+        if (counter > 2400):
+            break
+
             
