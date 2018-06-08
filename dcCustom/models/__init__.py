ce4e2540c43385555ce7e44f33fd989df27ad9e8
Qingyuan Feng <simonfengqy@gmail.com>
2018-06-08 11:33:31 -0700

    Add files via upload

--- dcCustom/models/__init__.py
+++ dcCustom/models/__init__.py
@@ -8,4 +8,4 @@ from __future__ import unicode_literals
 from dcCustom.models.models import Model
 from dcCustom.models.tensorgraph.tensor_graph import TensorGraph
 from dcCustom.models.tensorgraph.graph_models import WeaveModel, GraphConvModel, MPNNModel
-from dcCustom.models.tensorgraph.fcnet import MultiTaskRegressor, MultiTaskClassifier
+from dcCustom.models.tensorgraph.fcnet import MultitaskRegressor, MultitaskClassifier
