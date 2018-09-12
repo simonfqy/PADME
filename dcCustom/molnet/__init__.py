5e97ba97f1389ea975b196a31b3464ca2cd00512
Qingyuan Feng <simonfengqy@gmail.com>
2018-09-12 09:15:40 -0700

    Add files via upload

--- dcCustom/molnet/__init__.py
+++ dcCustom/molnet/__init__.py
@@ -9,3 +9,4 @@ from dcCustom.molnet.load_function.kinase_datasets import load_kinases
 from dcCustom.molnet.load_function.tc_kinase_datasets import load_tc_kinases
 from dcCustom.molnet.load_function.tc_full_kinase_datasets import load_tc_full_kinases
 from dcCustom.molnet.load_function.tc_dataset import load_toxcast
+from dcCustom.molnet.load_function.nci60_dataset import load_nci60
