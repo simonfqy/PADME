64323f947330390f7cef5f55ffdf4889ab64e3e6
Qingyuan Feng <simonfengqy@gmail.com>
2019-02-12 17:34:23 -0500

    Add files via upload

--- dcCustom/molnet/load_function/tc_dataset.py
+++ dcCustom/molnet/load_function/tc_dataset.py
@@ -10,7 +10,6 @@ import argparse
 import os
 import time
 import sys
-import pwd
 import pdb
 import csv
 import re
@@ -24,27 +23,38 @@ from dcCustom.molnet.check_availability import CheckFeaturizer, CheckSplit
 def load_toxcast(featurizer = 'Weave', cross_validation=False, test=False, split='random', 
   reload=True, K = 5, mode = 'regression', predict_cold = False, cold_drug=False, 
   cold_target=False, split_warm=False, filter_threshold=0, prot_seq_dict=None, currdir="./",
-  oversampled=False): 
+  oversampled=False, input_protein=True): 
   # The last parameter means only splitting into training and validation sets.
 
   if cross_validation:
     assert not test
+  data_dir = currdir + "full_toxcast/"  
+  if input_protein:
+    if mode == 'regression' or mode == 'reg-threshold':
+      mode = 'regression'      
+      file_name = "restructured.csv"
+    elif mode == 'classification':      
+      file_name = "restructured_bin.csv"
+    dataset_file = os.path.join(data_dir, file_name)
+    df = pd.read_csv(dataset_file, header = 0, index_col=False)
+    headers = list(df)
+    tasks=headers[:-3]
+  else:    
+    if mode == 'regression' or mode == 'reg-threshold':
+      mode = 'regression'
+      file_name = "restructured_no_prot.csv"
+    elif mode == 'classification':
+      file_name = "restructured_bin_no_prot.csv"
+    dataset_file = os.path.join(data_dir, file_name)
+    df = pd.read_csv(dataset_file, header = 0, index_col=False)
+    headers = list(df)    
+    tasks = headers[:-1]
 
-  if mode == 'regression' or mode == 'reg-threshold':
-    mode = 'regression'
-    file_name = "restructured_oversampled.csv"
-    
-  elif mode == 'classification':   
-    file_name = "restructured_bin.csv"
-
-  data_dir = currdir + "full_toxcast/"
-  dataset_file = os.path.join(data_dir, file_name)
-  df = pd.read_csv(dataset_file, header = 0, index_col=False)
-  headers = list(df)
-  tasks = headers[:-3]
   
   if reload:
     delim = "/"
+    if not input_protein:
+      delim = "_no_prot" + delim
     if filter_threshold > 0:
       delim = "_filtered" + delim
     if predict_cold:
@@ -55,6 +65,8 @@ def load_toxcast(featurizer = 'Weave', cross_validation=False, test=False, split
       delim = "_cold_drug" + delim
     elif cold_target:
       delim = "_cold_target" + delim
+    if oversampled:
+      delim = "_oversp" + delim
     if cross_validation:
       delim = "_CV" + delim
       save_dir = os.path.join(data_dir, featurizer + delim + mode + "/" + split)
@@ -62,7 +74,7 @@ def load_toxcast(featurizer = 'Weave', cross_validation=False, test=False, split
           save_dir, K)
     else:
       save_dir = os.path.join(data_dir, featurizer + delim + mode + "/" + split)
-      loaded, all_dataset, transformers = deepchem.utils.save.load_dataset_from_disk(
+      loaded, all_dataset, transformers = dcCustom.utils.save.load_dataset_from_disk(
           save_dir)
     if loaded:
       return tasks, all_dataset, transformers 
@@ -76,7 +88,8 @@ def load_toxcast(featurizer = 'Weave', cross_validation=False, test=False, split
   
   loader = dcCustom.data.CSVLoader(
       tasks = tasks, smiles_field="smiles", protein_field = "proteinName", 
-      source_field = 'protein_dataset', featurizer=featurizer, prot_seq_dict=prot_seq_dict)
+      source_field = 'protein_dataset', featurizer=featurizer, prot_seq_dict=prot_seq_dict,
+      input_protein = input_protein)
   dataset = loader.featurize(dataset_file, shard_size=8192)
   
   if mode == 'regression':
@@ -97,7 +110,7 @@ def load_toxcast(featurizer = 'Weave', cross_validation=False, test=False, split
       'index': deepchem.splits.IndexSplitter(),
       'random': dcCustom.splits.RandomSplitter(split_cold=predict_cold, cold_drug=cold_drug, 
         cold_target=cold_target, split_warm=split_warm, prot_seq_dict=prot_seq_dict,
-        threshold=filter_threshold, oversampled=oversampled),
+        threshold=filter_threshold, oversampled=oversampled, input_protein=input_protein),
       'scaffold': deepchem.splits.ScaffoldSplitter(),
       'butina': deepchem.splits.ButinaSplitter(),
       'task': deepchem.splits.TaskSplitter()
@@ -107,7 +120,7 @@ def load_toxcast(featurizer = 'Weave', cross_validation=False, test=False, split
     train, valid, test = splitter.train_valid_test_split(dataset)
     all_dataset = (train, valid, test)
     if reload:
-      deepchem.utils.save.save_dataset_to_disk(save_dir, train, valid, test,
+      dcCustom.utils.save.save_dataset_to_disk(save_dir, train, valid, test,
                                                transformers)
   elif cross_validation:
     fold_datasets = splitter.k_fold_split(dataset, K)
@@ -117,11 +130,11 @@ def load_toxcast(featurizer = 'Weave', cross_validation=False, test=False, split
 
   else:
     # not cross validating, and not testing.
-    train, valid, test = splitter.train_valid_test_split(dataset, frac_train=0.8, frac_valid=0.2,
+    train, valid, test = splitter.train_valid_test_split(dataset, frac_train=0.9, frac_valid=0.1,
       frac_test=0)
     all_dataset = (train, valid, test)
     if reload:
-      deepchem.utils.save.save_dataset_to_disk(save_dir, train, valid, test,
+      dcCustom.utils.save.save_dataset_to_disk(save_dir, train, valid, test,
                                                transformers)
   
   return tasks, all_dataset, transformers  
