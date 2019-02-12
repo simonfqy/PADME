42fd8e057ec8cf7e314448c3b5847f28623c75d5
Qingyuan Feng <simonfengqy@gmail.com>
2019-02-12 17:22:23 -0500

    Add files via upload

--- full_toxcast/preprocess.py
+++ full_toxcast/preprocess.py
@@ -25,16 +25,27 @@ protID_dict = dict(df_uniprot.iloc[:]['Uniprot_ID'])
 protList = list(set(df_uniprot.iloc[:]['Uniprot_ID']))
 num_subgroups = len(set(df_uniprot.iloc[:]['Subgroup']))
 
+df_mols = pd.read_csv('mol.csv', header = 0, index_col=False)
+mol_mapping = {}
+for i in range(len(df_mols)):
+  mol_mapping[df_mols.iloc[i][0]] = df_mols.iloc[i][1]
+
 
 def generate_data(input_csv, binarize=False, head_only=False, head_row_num=300, 
   limit_rows=False, limit_row_num=2400, prefix="toxcast_", input_prot=True, output_csv=None):
   
-  df = pd.read_csv('Assays_smiles_INCHI.csv', header = 0, index_col=2)
+  df = pd.read_csv(input_csv, header = 0, index_col=2)
   if head_only:
     df=df.head(head_row_num)
   assayList = list(df)[16:]
-  molList = list(df.index)
-  molList = [mol for mol in molList if mol==mol]
+  molList = []
+  for mol in list(df.index):
+    if mol != mol:
+      continue
+    if mol in mol_mapping:
+      mol = mol_mapping[mol]
+    molList.append(mol)    
+    
   row_ind = 0
   col_ind = 0
   INACTIVE_VAL = 1000.0
@@ -42,22 +53,27 @@ def generate_data(input_csv, binarize=False, head_only=False, head_row_num=300,
   max_value = 0
   inactive_counter = 0
   num_pairs = len(molList) * len(protList)
-  data_matrix = np.empty((num_pairs, num_subgroups))
+  if input_prot:
+    data_matrix = np.empty((num_pairs, num_subgroups))
+  else:
+    data_matrix = np.empty((len(molList), len(assayList)))
   data_matrix[:] = np.nan
   pair_list = []
   subgroup_list = []
+  smiles_set = set()
   pair_set = set()
   subgroup_set = set()
   # The (compound, protein) pair or group id are keys, and row or column indices are values.
   pair_to_row_ind = {}
   group_to_col_ind = {}
   na_counter = 0
-  goodval_counter = 0
   time_start_loop = time.time()
   for j in range(len(df)):
     this_row = dict(df.iloc[j])
     assert df.index[j] == df.index[j]
     smiles = molList[j]
+    assert smiles not in smiles_set
+    smiles_set.add(smiles)
     if j % 400 == 1:
       print("Now processing dataframe row: ", j + 1)
     for i, assay_name in enumerate(assayList):
@@ -65,41 +81,47 @@ def generate_data(input_csv, binarize=False, head_only=False, head_row_num=300,
       if entry_value != entry_value:
         na_counter += 1
         continue
-      prot_id = protID_dict[assay_name]
-      group_id = subgroup_dict[assay_name]
-      pair = (smiles, prot_id)
-
       if entry_value == 1000000:
-        entry_value = INACTIVE_VAL
-        inactive_counter += 1
+          entry_value = INACTIVE_VAL
+          inactive_counter += 1
       elif entry_value > max_value:
         max_value = entry_value
 
       if entry_value < min_val and entry_value != 0:
         min_val = entry_value
+      
+      if input_prot:
+        prot_id = protID_dict[assay_name]
+        group_id = subgroup_dict[assay_name]
+        pair = (smiles, prot_id)
+        
+        if pair not in pair_set:        
+          pair_to_row_ind[pair] = len(pair_list)
+          pair_list.append(pair)
+          pair_set.add(pair)
+                   
+        row_ind = pair_to_row_ind[pair]
 
-      if pair not in pair_set:        
-        pair_to_row_ind[pair] = len(pair_list)
-        pair_list.append(pair)
-        pair_set.add(pair)
-                 
-      row_ind = pair_to_row_ind[pair]
+        if group_id not in subgroup_set:
+          group_to_col_ind[group_id] = len(subgroup_list)
+          subgroup_list.append(group_id)
+          subgroup_set.add(group_id)        
+                
+        col_ind = group_to_col_ind[group_id]
 
-      if group_id not in subgroup_set:
-        group_to_col_ind[group_id] = len(subgroup_list)
-        subgroup_list.append(group_id)
-        subgroup_set.add(group_id)        
-              
-      col_ind = group_to_col_ind[group_id]
-
-      assert data_matrix[row_ind, col_ind] != data_matrix[row_ind, col_ind]
-      data_matrix[row_ind, col_ind] = entry_value
-      goodval_counter += 1
+        assert data_matrix[row_ind, col_ind] != data_matrix[row_ind, col_ind]
+        data_matrix[row_ind, col_ind] = entry_value        
+        
+      else:
+        assert data_matrix[j, i] != data_matrix[j, i]
+        data_matrix[j, i] = entry_value
+  
   time_end_loop = time.time()
   print("Time used in the loop: ", time_end_loop - time_start_loop)
   print("Minimum value: ", min_val)
-  data_matrix = data_matrix[:len(pair_list), :len(subgroup_list)]
-  # Now we have: na_counter: 4124417, inactive_counter: 954536, goodval_counter: 66551.
+  if input_prot:
+    data_matrix = data_matrix[:len(pair_list), :len(subgroup_list)]
+  # Now we have: na_counter: 4124417, inactive_counter: 954536, goodval_counter (already deleted): 66551.
 
   data_matrix = 4 - np.log10(data_matrix)
   max_val = 4 - np.log10(min_val)
@@ -111,7 +133,6 @@ def generate_data(input_csv, binarize=False, head_only=False, head_row_num=300,
     second_ind = inf_indices[1][i]
     assert np.isinf(data_matrix[first_ind, second_ind])
     data_matrix[first_ind, second_ind] = np.ceil(max_val)
-  # pdb.set_trace()
 
   # num_bins = 20
   # fig, ax = plt.subplots()
@@ -129,21 +150,46 @@ def generate_data(input_csv, binarize=False, head_only=False, head_row_num=300,
   # #pdb.set_trace()
 
   #counter = 0
-  shuffled = np.random.permutation(range(len(pair_list)))
-  start_writing = time.time()
-  subgroup_list = ['toxcast_' + str(task_id) for task_id in subgroup_list]
-  with open('restructured.csv', 'w', newline='') as csvfile:
-    fieldnames = subgroup_list + ['smiles', 'proteinName', 'protein_dataset']
+  shuffled_pairs = np.random.permutation(range(len(pair_list)))
+  start_writing = time.time()    
+  with open(output_csv, 'w', newline='') as csvfile:
+    fieldnames = ['smiles']
+    if input_prot:
+      subgroup_list = [prefix + str(task_id) for task_id in subgroup_list]
+      fieldnames = subgroup_list + fieldnames + ['proteinName', 'protein_dataset']
+    else:
+      col_names = [prefix + str(subgroup_dict[assay_name]) + '_' + assay_name for assay_name in assayList]
+      fieldnames = col_names + fieldnames
+      
     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
     writer.writeheader()
-    for i in range(len(pair_list)):
-      index = shuffled[i]
-      pair = pair_list[index]
-      data_line = ['' if np.isnan(entry) else entry for entry in data_matrix[index]]
-      line_values = dict(zip(subgroup_list, data_line))
-      out_line = {'smiles': pair[0], 'proteinName': pair[1], 'protein_dataset': 'toxcast'}
-      line_values.update(out_line)
-      writer.writerow(line_values)
+    if input_prot:
+      for i in range(len(pair_list)):
+        if limit_rows and i > limit_row_num - 1:
+          break
+        index = shuffled_pairs[i]
+        pair = pair_list[index]
+        data_line = ['' if np.isnan(entry) else entry for entry in data_matrix[index]]
+        line_values = dict(zip(subgroup_list, data_line))
+        out_line = {'smiles': pair[0], 'proteinName': pair[1], 'protein_dataset': 'toxcast'}
+        line_values.update(out_line)
+        writer.writerow(line_values)
+        
+    else:
+      shuffled_mol_list = np.random.permutation(range(len(molList)))      
+
+      for ind in range(len(molList)):
+        if limit_rows and ind > limit_row_num - 1:
+          break
+        i = shuffled_mol_list[ind]
+        output_dict = {'smiles': molList[i]}
+        for j, assay_name in enumerate(assayList):
+          intxn_value = data_matrix[i, j]
+          if intxn_value != intxn_value:
+            intxn_value = ''
+          col_name = prefix + str(subgroup_dict[assay_name]) + '_' + assay_name
+          output_dict[col_name] = intxn_value
+        writer.writerow(output_dict)
 
   end_writing = time.time()
   print("Time spent in writing: ", end_writing - start_writing)
@@ -220,5 +266,6 @@ def oversample(input_file, output_file, direction=True, inactive_threshold = 1.0
 
 
 if __name__ == "__main__":
+  generate_data('Assays_smiles_INCHI.csv', input_prot=False, output_csv='restructured_no_prot.csv')
   # oversample('restructured.csv', 'restructured_oversampled.csv')
-  generate_data()
+  
